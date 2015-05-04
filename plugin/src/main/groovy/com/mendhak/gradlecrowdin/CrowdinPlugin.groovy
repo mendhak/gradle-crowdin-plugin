d12d33e8006f3fbb4f7ce6abcdc5033206cf3898
mendhak <mendhak@users.noreply.github.com>
2015-05-04 08:35:20 +0100

    Added description

--- plugin/src/main/groovy/com/mendhak/gradlecrowdin/CrowdinPlugin.groovy
+++ plugin/src/main/groovy/com/mendhak/gradlecrowdin/CrowdinPlugin.groovy
@@ -5,7 +5,7 @@ import org.gradle.api.Plugin
 
 class CrowdinPlugin implements Plugin<Project> {
     void apply(Project target) {
-        target.task('crowdinDownload', type: DownloadTranslationsTask, group:'crowdin')
-        target.task('crowdinUpload', type: UploadSourceFileTask, group:'crowdin')
+        target.task('crowdinDownload', type: DownloadTranslationsTask, group:'crowdin', description:'Download and copy translated files to given destination')
+        target.task('crowdinUpload', type: UploadSourceFileTask, group:'crowdin', description: 'Update a given source file on crowdin')
     }
 }
